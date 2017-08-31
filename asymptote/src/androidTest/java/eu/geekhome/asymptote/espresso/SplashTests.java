5e1eb82eb56781ea69c4d67d9cbbd7f8cad25eff
agaitomek <tomasz.babiuk@gmail.com>
2017-08-31 10:02:20 +0200

    adding espresso to the project

---
+++ asymptote/src/androidTest/java/eu/geekhome/asymptote/espresso/SplashTests.java
@@ -0,0 +1,47 @@
+package eu.geekhome.asymptote.espresso;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import eu.geekhome.asymptote.R;
+import eu.geekhome.asymptote.SplashActivity;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SplashTests {
+
+    private String mStringToBetyped;
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mActivityRule = new ActivityTestRule<>(
+            SplashActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        onView(withText("dupa")).check(matches(withText("dupa")));
+    }
+}
